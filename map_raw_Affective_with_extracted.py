7c08a820ec60f6075d28637867aca250e1edae01
CalebUAz <calebshibu@email.arizona.edu>
2023-05-29 12:56:00 -0700

    store matched files in a variable

--- map_raw_Affective_with_extracted.py
+++ map_raw_Affective_with_extracted.py
@@ -5,7 +5,7 @@ import argparse
 def match_files(raw_folder, extracted_folder):
     raw_files = []
     extracted_files = []
-    matched_files = []
+    matched_files = {}
 
     # Get the list of CSV files in the raw experiment folder
     raw_affective_folder = os.path.join(raw_folder, "baseline_tasks", "affective")
@@ -23,19 +23,11 @@ def match_files(raw_folder, extracted_folder):
             raw_file_number = match.group(1).lstrip("0")
             for extracted_file in extracted_files:
                 extracted_file_number = os.path.splitext(extracted_file)[0]
-                if extracted_file_number.isdigit():
-                    # print(f"Comparing: raw_file_number={raw_file_number}, extracted_file_number={extracted_file_number}")
-                    if int(extracted_file_number) == int(raw_file_number):
-                        matched_files.append(raw_file)
-                        break
+                if extracted_file_number.isdigit() and int(extracted_file_number) == int(raw_file_number):
+                    matched_files[raw_file] = extracted_file
+                    break
 
-    # Print the matched files
-    if matched_files:
-        print("Matched files:")
-        for file in matched_files:
-            print(file)
-    else:
-        print("No matching files found.")
+    return matched_files
 
 def main():
     parser = argparse.ArgumentParser(description="Match files between raw and extracted experiment folders.")
@@ -47,7 +39,15 @@ def main():
     extracted_folder_path = args.extracted_folder
 
     # Call the function to match the files
-    match_files(raw_folder_path, extracted_folder_path)
+    matched_files = match_files(raw_folder_path, extracted_folder_path)
+    print(matched_files)
+    # Print the matched files
+    # if matched_files:
+    #     print("Matched files:")
+    #     for raw_file, extracted_file in matched_files.items():
+    #         print(f"Raw File: {raw_file} | Extracted File: {extracted_file}")
+    # else:
+    #     print("No matching files found.")
 
 if __name__ == "__main__":
     main()
