f15b399c1583f864f235c5c44902c931037bc291
CalebUAz <calebshibu@email.arizona.edu>
2023-05-28 17:21:27 -0700

    debug

--- list_exp_good_NIRS.sh
+++ list_exp_good_NIRS.sh
@@ -4,11 +4,9 @@
 
 # Usage: ./script.sh -p /path/to/directory
 
-# Parse command line arguments
 while getopts ":p:" opt; do
   case ${opt} in
     p ) 
-      # Assign the provided path to a variable
       parent_directory=$OPTARG
       ;;
     \? ) 
@@ -17,28 +15,29 @@ while getopts ":p:" opt; do
   esac
 done
 
-# If no directory is provided, display a usage message
 if [ -z "${parent_directory}" ]; then
     echo "You must provide a directory path with -p."
     exit 1
 fi
 
-# Change directory to the parent_directory
 cd "${parent_directory}"
 
-# Iterate over each experiment directory
 for exp_folder in exp_*; do
   all_good=true
-  # Iterate over each animal folder
   for animal in lion tiger leopard; do
     csv_file="${exp_folder}/${animal}/NIRS_channel_quality.csv"
-    # Check if all status are good_channel using awk
-    if ! awk -F',' '{if (NR>1 && $3 != "good_channel") exit 1}' "${csv_file}"; then
+    # Check if the file exists before processing it with awk
+    if [ -f "${csv_file}" ]; then
+      if ! awk -F',' '{if (NR>1 && $3 != "good_channel") exit 1}' "${csv_file}"; then
+        all_good=false
+        break
+      fi
+    else
+      echo "File not found: ${csv_file}"
       all_good=false
       break
     fi
   done
-  # If all statuses in all csv files are good_channel, print the exp_folder path
   if ${all_good}; then
     echo "${parent_directory}/${exp_folder}"
   fi
