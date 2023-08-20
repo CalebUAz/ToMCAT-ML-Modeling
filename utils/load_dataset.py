69e95a2c5ee67d832949964423d6048fdb7226f5
CalebUAz <calebshibu@email.arizona.edu>
2023-08-19 23:18:37 -0700

    update script for eeg

--- utils/load_dataset.py
+++ utils/load_dataset.py
@@ -87,36 +87,75 @@ def load_dataset_NIRS(path):
     return combined_df
 
 def load_dataset_EEG(path):
+    print("-------------------------------------------------------------------------------------------------")
+    print("Reading EEG affective task data from: {}".format(path))
+    print("-------------------------------------------------------------------------------------------------")
+    
+    # Read ignore experimenter file
+    ignore_df = pd.read_csv('utils/ignore_experimenter.csv')
+    
     directory = path
     dfs = []
     headers = [
     "AFF1h", "F7", "FC5", "C3", "T7", "TP9", "Pz", "P3", "P7", "O1", "O2", "P8", "P4", "TP10", "Cz", "C4", "T8", "FC6", "FCz", "F8", "AFF2h", "GSR", "EKG", "arousal_score", "valence_score"]
 
+    drop_headers = ['AFF5h', 'FC1', 'CP5', 'CP1', 'PO9', 'Oz', 'PO10', 'CP6', 'CP2', 'FC2', 'AFF6h']
+    
     columns_to_keep = ["FC5", "FCz", "FC6", "F7",
                        "F8", "AFF1h", "AFF2h", "arousal_score", "valence_score"]
-
+    count = 0
     for root, dirs, files in os.walk(directory):
         for folder in dirs:
             if folder.startswith('exp_'):
                 folder_path = os.path.join(root, folder)
                 
                 for file in os.listdir(folder_path):
-                    if file.endswith('affective_individual_physio_task.csv'):
+                    if file.startswith('affective_individual_'):
                         file_path = os.path.join(folder_path, file)
+
+                        # Check if file path contains an ignore combination
+                        station = file.split('_')[-1].split('.')[0] # extract station name from the file name
+                        ignore_rows = ignore_df[(ignore_df['group_session'] == folder) & (ignore_df['station'] == station)]
+                        if not ignore_rows.empty:
+                            print(f"Ignoring file {file_path} because experimenter was sitting there")
+                            continue # Skip the rest and move to the next file
                         
                         df = pd.read_csv(file_path)
                         imac = os.path.basename(file).split('_')[0]
-                        df.drop(columns=['unix_time', 'task_time', 'task_monotonic_time', 'task_human_readable_time', 'task_subject_id', 'seconds_since_start', 'human_readable_time', imac, 'task_index', 'experiment_id'], inplace=True)
+                        imac_id = imac + '_id'
+                        df.drop(columns=['timestamp_unix', 'station'], inplace=True)
+
+                        cols_to_drop = [col for col in df.columns if any(col.endswith(header) for header in drop_headers)]
+                        df.drop(columns=cols_to_drop, inplace=True) #The number of channels are different before 11/2022
+                         
                         if 'task_Unnamed: 0' in df.columns:
                             df.drop(columns='task_Unnamed: 0', inplace=True)
 
-                        df.loc[df['task_event_type'].isin(['intermediate_selection','show_cross_screen', 'show_image', 'show_rating_screen']), ['task_arousal_score', 'task_valence_score']] = np.nan
-                        df[['task_image_path', 'task_arousal_score', 'task_valence_score', 'task_event_type']] = df[['task_image_path', 'task_arousal_score', 'task_valence_score', 'task_event_type']].fillna(method='bfill')
-                        df.drop(columns=['task_image_path', 'task_event_type'], inplace=True)
+                        if imac in df.columns:
+                            df.drop(imac, inplace=True)
+                            
+                        if imac_id in df.columns:
+                            df.drop(columns= imac_id, inplace=True)
+
+                        df.loc[df['event_type'].isin(['intermediate_selection','show_cross_screen', 
+                                                      'show_image', 'show_rating_screen']), 
+                                                    ['arousal_score', 'valence_score']] = np.nan
+                        df[['image_path', 'arousal_score', 
+                            'valence_score', 'event_type']] = df[['image_path', 'arousal_score', 
+                                                                        'valence_score', 'event_type']].fillna(method='bfill')
+                        
+                        df.drop(columns=['image_path', 'event_type'], inplace=True)
                         df.dropna(inplace=True)
                         df.columns = [None] * len(df.columns)
-                        dfs.append(df)
-
+                        
+                        if df.shape[1] > 2:
+                            dfs.append(df)
+                            count += 1
+                        #else:
+                            #print(df.shape, file_path)
+    print("-------------------------")
+    print("Number of subjects: {}".format(count))
+    print("-------------------------")
     combined_df = pd.concat(dfs, ignore_index=True)
     combined_df = combined_df.set_axis(headers, axis=1)
     combined_df = combined_df[columns_to_keep]
