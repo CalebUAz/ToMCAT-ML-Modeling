58ba8369c5453eb1aa19c5685349daf3398eb99c
CalebUAz <calebshibu@email.arizona.edu>
2023-09-11 20:55:48 -0700

    debug

--- CNN/utils/save_plot_with_timestamp.py
+++ CNN/utils/save_plot_with_timestamp.py
@@ -2,6 +2,7 @@ import os
 from datetime import datetime
 
 def save_plot_with_timestamp(plt, title, folder_path):
+    print("Saving plot...")
     current_time = datetime.now().strftime("%Y%m%d%H%M%S")
     file_name = f'{current_time}_{title}.png'
     file_path = os.path.join(folder_path, file_name)
