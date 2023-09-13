c7ce903015f658fd74ea649375fa6e7c5f96c3a0
CalebUAz <calebshibu@email.arizona.edu>
2023-09-12 21:58:12 -0700

    model updated with BatchNorm2d and  Dropout

--- CNN/NIRS_affective_individual_task_CNN.py
+++ CNN/NIRS_affective_individual_task_CNN.py
@@ -86,18 +86,31 @@ def classify_CNN_Affective_Individual_Task_NIRS(path, hidden_size, num_epochs, b
             super(CNN, self).__init__()
             
             self.conv1 = nn.Conv2d(1, 32, kernel_size=(3, 3), stride=1, padding=1)
-            self.conv2 = nn.Conv2d(32, 64, kernel_size=(3, 3), stride=1, padding=1)
-            self.conv3 = nn.Conv2d(64, 128, kernel_size=(3, 3), stride=1, padding=1)
-            self.fc1 = nn.Linear(input_shape[0] * input_shape[1] * 128, 128)  # Adjusted for 2D input
+            self.bn1 = nn.BatchNorm2d(32)
+            self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+            
+            self.conv2 = nn.Conv2d(32, 64, kernel_size=(5, 5), stride=1, padding=2)
+            self.bn2 = nn.BatchNorm2d(64)
+            self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+            
+            self.conv3 = nn.Conv2d(64, 128, kernel_size=(7, 7), stride=1, padding=3) 
+            self.bn3 = nn.BatchNorm2d(128)
+            self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+            
+            self.drop = nn.Dropout(0.5)
+            
+            reduced_dim = input_shape[0]//8 * input_shape[1]//8 * 128  # Assuming pooling is done three times
+            self.fc1 = nn.Linear(reduced_dim, 128)  
             self.fc_arousal = nn.Linear(128, num_classes)
             self.fc_valence = nn.Linear(128, num_classes)
             
         def forward(self, x):
-            x = nn.ReLU()(self.conv1(x))
-            x = nn.ReLU()(self.conv2(x))
-            x = nn.ReLU()(self.conv3(x))
+            x = self.pool1(nn.ReLU()(self.bn1(self.conv1(x))))
+            x = self.pool2(nn.ReLU()(self.bn2(self.conv2(x))))
+            x = self.pool3(nn.ReLU()(self.bn3(self.conv3(x))))
+            
             x = x.view(x.size(0), -1)  # Flatten the tensor
-            x = nn.ReLU()(self.fc1(x))
+            x = self.drop(nn.ReLU()(self.fc1(x)))
             arousal = self.fc_arousal(x)
             valence = self.fc_valence(x)
             return arousal, valence
