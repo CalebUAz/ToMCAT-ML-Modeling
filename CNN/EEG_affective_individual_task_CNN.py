c38d5c964afa18bb4e16295afc559edd13ca9a5e
CalebUAz <calebshibu@email.arizona.edu>
2023-09-17 23:27:06 -0700

    .

--- CNN/EEG_affective_individual_task_CNN.py
+++ CNN/EEG_affective_individual_task_CNN.py
@@ -82,7 +82,6 @@ def classify_CNN_Affective_Individual_Task_EEG(path, hidden_size, num_epochs, ba
     num_folds = 5
 
     # Create DataLoaders
-    features = features.permute(0, 2, 1).unsqueeze(-1)
     dataset = TensorDataset(torch.tensor(features).float().to(device), torch.tensor(targets).long().to(device))
     if subject_holdout:
         # Use 80% of the subjects for training
