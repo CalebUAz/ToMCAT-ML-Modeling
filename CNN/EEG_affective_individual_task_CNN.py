63dbd687a68bd9d9dc3e6d7326c6cae821cfc612
CalebUAz <calebshibu@email.arizona.edu>
2023-09-17 23:08:53 -0700

    data shaped (17549, 500, 7), you'll need to reshape it to (17549, 7, 500, 1) for it to work with the 2D convolutional layers

--- CNN/EEG_affective_individual_task_CNN.py
+++ CNN/EEG_affective_individual_task_CNN.py
@@ -82,6 +82,7 @@ def classify_CNN_Affective_Individual_Task_EEG(path, hidden_size, num_epochs, ba
     num_folds = 5
 
     # Create DataLoaders
+    features = features.permute(0, 2, 1).unsqueeze(-1)
     dataset = TensorDataset(torch.tensor(features).float().to(device), torch.tensor(targets).long().to(device))
     if subject_holdout:
         # Use 80% of the subjects for training
