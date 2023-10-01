6f23a34563db60e9b0b378a1d08e916c5def3855
CalebUAz <calebshibu@email.arizona.edu>
2023-09-30 22:56:17 -0700

    revert back to calculating wavelet from band data

--- CNN/utils/extract_EEG_features.py
+++ CNN/utils/extract_EEG_features.py
@@ -23,35 +23,41 @@ def get_eeg_frequency_band_data(signals, use_wavelet, use_emd):
     all_band_data = []
     all_wavelet_data = []
     all_imf_data = []
-
+    
     wavelet = 'db4'  # Daubechies 4 wavelet
     level = 4  # Level of decomposition
 
     for band, (fmin, fmax) in bands.items():
         filtered_data = raw.copy().filter(fmin, fmax).get_data()
         all_band_data.append(filtered_data)
+        
+        if use_wavelet:
+            band_wavelet_data = []
+            for channel_data in filtered_data:
+                coeffs = pywt.wavedec(channel_data, wavelet, level=level)
+                reconstructed_signal = pywt.waverec(coeffs, wavelet)
+                if len(reconstructed_signal) != len(channel_data):
+                    reconstructed_signal = reconstructed_signal[:len(channel_data)]
+                band_wavelet_data.append(reconstructed_signal)
+            all_wavelet_data.append(np.array(band_wavelet_data))
 
-    if use_wavelet:
-        for channel_data in signals:
-            coeffs = pywt.wavedec(channel_data, wavelet, level=level)
-            reconstructed_signal = pywt.waverec(coeffs, wavelet)
-            if len(reconstructed_signal) != len(channel_data):
-                reconstructed_signal = reconstructed_signal[:len(channel_data)]
-            all_wavelet_data.append(reconstructed_signal)
-
-    if use_emd:
-        for channel_data in signals:
-            emd = EMD()
-            imfs = emd(channel_data)
-            first_four_imfs = imfs[:4, :] if imfs.shape[0] >= 4 else np.vstack((imfs, np.zeros((4 - imfs.shape[0], imfs.shape[1]))))
-            all_imf_data.append(first_four_imfs)
+        if use_emd:
+            band_imf_data = []
+            for channel_data in filtered_data:
+                emd = EMD()
+                imfs = emd(channel_data)
+                first_four_imfs = imfs[:4, :] if imfs.shape[0] >= 4 else np.vstack((imfs, np.zeros((4 - imfs.shape[0], imfs.shape[1]))))
+                band_imf_data.append(first_four_imfs)
+            all_imf_data.append(np.array(band_imf_data))
 
     stacked_band_data = np.concatenate(all_band_data, axis=0)
     
     combined_data = stacked_band_data
     if use_wavelet:
-        combined_data = np.concatenate((combined_data, np.array(all_wavelet_data)), axis=0)
+        stacked_wavelet_data = np.concatenate(all_wavelet_data, axis=0)
+        combined_data = np.concatenate((combined_data, stacked_wavelet_data), axis=0)
     if use_emd:
-        combined_data = np.concatenate((combined_data, np.vstack(all_imf_data)), axis=0)
+        stacked_imf_data = np.concatenate(all_imf_data, axis=0)
+        combined_data = np.concatenate((combined_data, stacked_imf_data), axis=0)
         
     return combined_data.T
