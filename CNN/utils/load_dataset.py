19e432552103a71db470efa71dd360f8e39d118b
CalebUAz <calebshibu@email.arizona.edu>
2023-10-05 00:40:19 -0700

    Offset by 30 samples and then look at 10 samples

--- CNN/utils/load_dataset.py
+++ CNN/utils/load_dataset.py
@@ -114,7 +114,7 @@ def load_dataset_NIRS(path):
 
     else:
         # Offset and length definitions
-        offset = 20
+        offset = 30
         length = 10
         # Calculate cumulative counts
         combined_df_temp['cumcount'] = combined_df_temp.groupby(['subject_id', 'image_path']).cumcount()
