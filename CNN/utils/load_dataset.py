d9a81704e4933383c66c7b0131a6b05cf48f6599
CalebUAz <calebshibu@email.arizona.edu>
2023-10-05 19:26:25 -0700

    offset = 2000         length = 500

--- CNN/utils/load_dataset.py
+++ CNN/utils/load_dataset.py
@@ -233,7 +233,7 @@ def load_dataset_EEG(path):
 
     else:
         # Offset and length definitions
-        offset = 1000
+        offset = 2000
         length = 500
         # Calculate cumulative counts
         combined_df_temp['cumcount'] = combined_df_temp.groupby(['subject_id', 'image_path']).cumcount()
